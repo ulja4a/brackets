50471d96a31f724fe11b2c5f78df442fe20e6b2e
ulja4a <ulja4a@gmail.com>
2023-09-29 14:01:41 +0400

    failed tests when brackets open and close are the same

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let joinedStr = str.split('');
+  let stack = [];
+  let openBrackets = bracketsConfig.map(pair => pair[0]);
+  let closeBrackets = bracketsConfig.map(pair => pair[1]);
+
+  if ((joinedStr.length % 2) !== 0) {
+    return false;
+  }
+
+  for (i=0; i<joinedStr.length; i++) {
+    let openIndex = openBrackets.indexOf(joinedStr[i]);
+    if (openIndex !==-1) {
+      stack.push(openIndex);
+      continue;
+    }
+    let closeIndex = closeBrackets.indexOf(joinedStr[i]);
+    if (closeIndex !==-1) {
+      openIndex = stack.pop();
+    if (closeIndex !== openIndex) {
+        return false;
+        
+      }
+    }
+  }
+    if (stack.length !== 0) {
+      return false;
+    }
+    return true;
 }
