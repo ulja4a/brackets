93defb1f103bf0f178e247fd5e2d3f682fe89ad5
ulja4a <ulja4a@gmail.com>
2023-09-29 16:24:31 +0400

    add identical brackets condition

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  let joinedStr = str.split('');
   let stack = [];
-  let openBrackets = bracketsConfig.map(pair => pair[0]);
-  let closeBrackets = bracketsConfig.map(pair => pair[1]);
+  let openBrackets = [];
+  let closeBrackets = [];
 
-  if ((joinedStr.length % 2) !== 0) {
-    return false;
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
   }
 
-  for (i=0; i<joinedStr.length; i++) {
-    let openIndex = openBrackets.indexOf(joinedStr[i]);
-    if (openIndex !==-1) {
-      stack.push(openIndex);
-      continue;
-    }
-    let closeIndex = closeBrackets.indexOf(joinedStr[i]);
-    if (closeIndex !==-1) {
-      openIndex = stack.pop();
-    if (closeIndex !== openIndex) {
-        return false;
-        
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    
+    if (openBrackets.includes(char) && closeBrackets.includes(char)) {
+      
+      if (stack.length === 0 || stack[stack.length - 1] !== char) {
+        stack.push(char);
+      } else {
+        stack.pop();
       }
+    } else {
+      let openIndex = openBrackets.indexOf(char);
+
+      if (openIndex !== -1) {
+        
+        stack.push(char);
+      } else {
+        let closeIndex = closeBrackets.indexOf(char);
+
+        if (closeIndex !== -1) {
+          
+          if (stack.length === 0) {
+            return false; 
+          }
+
+          let lastOpenBracket = stack.pop();
+
+          if (lastOpenBracket !== openBrackets[closeIndex]) {
+            return false; 
+          }
+         }
+       }
     }
   }
-    if (stack.length !== 0) {
-      return false;
-    }
-    return true;
+
+  return stack.length === 0; 
 }
